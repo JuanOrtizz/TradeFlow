e96ffb80dfa25fa90945e4a1cf64f2fccc46e803
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-08-18 12:15:07 -0300

    feat: Agrego servicio basico de validaciones de campos

---
+++ Services/IValidacionesService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TradeFlow.Services
+{
+    public interface IValidacionesService
+    {
+        // Metodo para validar que un campo no este vacio
+        string ValidarCampoVacio(string campo);
+    }
+}
---
+++ Services/ValidacionesService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TradeFlow.Services
+{
+    public class ValidacionesService : IValidacionesService
+    {
+        // Metodo para validar que un campo no este vacio
+        public string ValidarCampoVacio(string campo)
+        {
+            if (string.IsNullOrWhiteSpace(campo))
+            {
+                return "El campo está vacío.";
+            }
+            return string.Empty;
+        }
+    }
+}
