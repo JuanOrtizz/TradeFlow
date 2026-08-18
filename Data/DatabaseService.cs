384bbca28c455862be1ce0844d6d4b0c05ae3a1a
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-08-18 11:08:15 -0300

    feat: Actualizo DatabaseService

--- Data/DatabaseService.cs
+++ Data/DatabaseService.cs
@@ -1,37 +1,31 @@
-﻿using Bumptech.Glide.Load.Model;
-using SQLite;
+﻿using SQLite;
 using TradeFlow.Models;
 
-namespace TrainiumNeon.Data
+namespace TradeFlow.Data
 {
     public class DatabaseService
     {
-        // Propiedades privadas
         private readonly SQLiteAsyncConnection _db;
         private bool _initialized;
 
-        // Constructor
         public DatabaseService(string dbPath)
         {
             _db = new SQLiteAsyncConnection(dbPath);
         }
 
-        // Metodo publico para obtener la conexion a la DB
         public SQLiteAsyncConnection Connection()
         {
             return _db;
         }
 
-        // Task asincrona para inicializar la DB
         public async Task InitializeAsync()
         {
-            // Si la db ya esta inicializada salgo del metodo
+
             if (_initialized)
             {
                 return;
             }
 
-            // Creo las tablas de la DB
             await _db.CreateTableAsync<LocalidadModel>();
             await _db.CreateTableAsync<ClienteModel>();
             await _db.CreateTableAsync<ProductoModel>();
@@ -41,6 +35,5 @@ namespace TrainiumNeon.Data
             // Inicializo la variable para no volver a inicializar
             _initialized = true;
         }
-
     }
 }
