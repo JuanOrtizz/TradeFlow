81a1aa1e5fc2829f52d5d4f9b04b0cf27a50bf85
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-08-19 13:30:27 -0300

    feat: Agrego ViewModel y Code-Behind de Inicio

--- ViewModels/InicioViewModel.cs
+++ ViewModels/InicioViewModel.cs
@@ -1,6 +1,84 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TradeFlow.Data.Repositories;
+using TradeFlow.Models;
+using TradeFlow.Services;
+
 namespace TradeFlow.ViewModels
 {
     public class InicioViewModel
     {
+        //Servicios y repositorios
+        private readonly IFacturaRepository _facturaRepository;
+        private readonly IDisplayAlertService _displayAlertService;
+
+        // Propiedades privadas
+        private ObservableCollection<FacturaModel> _listaFacturas = new ObservableCollection<FacturaModel>();
+        private bool _isBusy;
+
+        // Propiedades públicas
+        public ObservableCollection<FacturaModel> ListaFacturas
+        {
+            get => _listaFacturas;
+            set
+            {
+                if(_listaFacturas != value)
+                {
+                    _listaFacturas = value;
+                    OnPropertyChanged(nameof(ListaFacturas));
+                }
+            }
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                if(_isBusy != value)
+                {
+                    _isBusy = value;
+                    OnPropertyChanged(nameof(IsBusy));
+                }
+            }
+        }
+
+        // Constructor
+        public InicioViewModel(IFacturaRepository facturaRepository, IDisplayAlertService displayAlertService)
+        {
+            _facturaRepository = facturaRepository;
+            _displayAlertService = displayAlertService;
+        }
+
+        public async Task InicializarAsync()
+        {
+            try
+            {
+                // Muestro spinner de carga
+                IsBusy = true;
+                _listaFacturas.Clear();
+                // Obtengo todas las facturas
+                var facturas = await _facturaRepository.ObtenerUltimasDiezAsync();
+                foreach (var factura in facturas)
+                {
+                    _listaFacturas.Add(factura);
+                }
+            }
+            catch (Exception)
+            {
+                await _displayAlertService.MostrarAlertAsync("Error", "No se pudo inicializar el menú principal", "OK");
+            }
+            finally
+            {
+                // Oculto spinner de carga
+                IsBusy = false;
+            }
+        }
+
+        // Implementacion de INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- Views/InicioView.xaml.cs
+++ Views/InicioView.xaml.cs
@@ -1,9 +1,28 @@
+using TradeFlow.ViewModels;
+
 namespace TradeFlow.Views;
 
 public partial class InicioView : ContentPage
 {
-    public InicioView()
+    private bool _inicializado = false;
+
+    public InicioView(InicioViewModel vm)
     {
         InitializeComponent();
+        BindingContext = vm;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (!_inicializado)
+        {
+            _inicializado = true;
+            if (BindingContext is InicioViewModel vm)
+            {
+                await vm.InicializarAsync();
+            }
+        }
     }
 }
